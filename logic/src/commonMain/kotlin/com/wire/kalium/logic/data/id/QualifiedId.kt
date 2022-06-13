965d01b5bf660f5300a8257ba9f8e63a1c02daa1
alexandreferris <ferris.alexandre@gmail.com>
2022-06-13 16:52:36 +0200

    chore: remove domain from toString and add as a todo with correct federation verification

--- logic/src/commonMain/kotlin/com/wire/kalium/logic/data/id/QualifiedId.kt
+++ logic/src/commonMain/kotlin/com/wire/kalium/logic/data/id/QualifiedId.kt
@@ -14,7 +14,8 @@ data class QualifiedID(
         const val WIRE_PRODUCTION_DOMAIN = "wire.com"
     }
 
-    override fun toString(): String = if (domain.isEmpty()) value else "$value$VALUE_DOMAIN_SEPARATOR$domain"
+    // TODO: Add proper federation verification whether to add or remove the domain.
+    override fun toString(): String = value
 }
 
 const val VALUE_DOMAIN_SEPARATOR = "@"
